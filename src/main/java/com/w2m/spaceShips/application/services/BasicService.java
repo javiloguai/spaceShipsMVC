e1f776009616cf934238c13d2ab420d395653ab8
javiloguai <javiloguai.xaic@gmail.com>
2024-10-27 17:35:47 +0100

    Fixing domain structure

--- src/main/java/com/w2m/spaceShips/application/services/BasicService.java
+++ src/main/java/com/w2m/spaceShips/application/services/BasicService.java
@@ -1,10 +1,6 @@
 package com.w2m.spaceShips.application.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.ApplicationContext;
-
-import java.util.Map;
-import java.util.Objects;
 
 /**
  * @author javiloguai
@@ -17,20 +13,6 @@ public class BasicService {
 
     public BasicService(final ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
-    }
-
-    /**
-     * Checks if an Object have a at least minimum fields with value
-     *
-     * @param object        The object to validate
-     * @param minimumFields The minimum fields
-     * @return the validation result
-     */
-    public boolean haveMinimumFields(final Object object, final Integer minimumFields) {
-        final ObjectMapper oMapper = new ObjectMapper();
-        final Map<String, Object> map = oMapper.convertValue(object, Map.class);
-        map.values().removeIf(Objects::isNull);
-        return map.size() >= minimumFields;
     }
 
 }
